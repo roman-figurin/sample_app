a999886dc4dc94f43274edc84a1fc72c0b075a93
Roman Figurin <r.v.figurin@gmail.com>
2019-02-15 11:32:36 +0300

    7.4.1 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successfull save.
+      redirect_to @user
     else
       render 'new'
     end
